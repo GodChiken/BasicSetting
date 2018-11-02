6079ef13f3afe61b18c3b9db851aedf25f772c80
masterkbh <godchiken@naver.com>
2018-11-02 16:44:38 +0900

    com.fasterxml.jackson.core:jackson-databind 보안 문제 해결

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # BasicSetting
 Spring Framework &amp; JPA 기본 설정
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/kbh/desk/presentation/controller/general/IndexController.java
+++ src/main/java/com/kbh/desk/presentation/controller/general/IndexController.java
@@ -1,10 +1,21 @@
 package com.kbh.desk.presentation.controller.general;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpSession;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 
 @Controller
@@ -14,11 +25,19 @@ public class IndexController {
     @RequestMapping("/")
     public ModelAndView index(HttpSession httpSession) {
         ModelAndView mav = new ModelAndView("index");
+
+
+
         return mav;
     }
+
     @RequestMapping("/test")
     public ModelAndView test(HttpSession httpSession) {
         ModelAndView mav = new ModelAndView("test");
+        AsyncRestTemplate rest = new AsyncRestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+
+
         return mav;
     }
 }
